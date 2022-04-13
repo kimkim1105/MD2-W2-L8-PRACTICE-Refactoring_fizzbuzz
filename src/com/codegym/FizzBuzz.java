c07f0b74060ff0ece51eba256777f9061cf5409a
unknown <huenk105@gmail.com>
2022-04-13 22:13:12 +0700

    demo

---
+++ src/com/codegym/FizzBuzz.java
@@ -0,0 +1,18 @@
+package com.codegym;
+
+public class FizzBuzz {
+    public static String fizzBuzz(int number){
+        boolean b = number % 3 == 0;
+        boolean b1 = number % 5 == 0;
+        if(b && b1)
+            return "FizzBuzz";
+
+        if(b)
+            return "Fizz";
+
+        if(b1)
+            return "Buzz";
+
+        return number + "";
+    }
+}
